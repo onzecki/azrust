20071cdffa0b42131f08462a9c6c442b6f9674fb
Mircea-Sebastian Turtureanu <90131332+turtureanu@users.noreply.github.com>
2023-09-30 23:40:10 +0200

    doc: improve code readability

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use walkdir::{DirEntry, WalkDir};
-use regex::{Regex, RegexBuilder};
-use clap::Parser;
+use regex::RegexBuilder;
+use clap::{CommandFactory, Parser};
 use std::fs;
 use std::time::SystemTime;
 use chrono::{DateTime, Utc};
-use serde::Serialize;
+use walkdir::{DirEntry, WalkDir};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -27,11 +26,7 @@ struct Args {
     hidden: bool,
 }
 
-#[derive(Serialize)]
-struct PathInfo {
-    path: String,
-}
-
+// Check if the &DirEntry is hidden
 fn is_hidden(entry: &DirEntry) -> bool {
     entry.file_name()
         .to_str()
@@ -39,45 +34,35 @@ fn is_hidden(entry: &DirEntry) -> bool {
         .unwrap_or(false)
 }
 
-fn matches_requirements(entry: &DirEntry, pattern: &Regex) -> bool {
-    entry.file_name()
-        .to_str()
-        .map(|s| pattern.is_match(s))
-        .unwrap_or(false)
-}
-
+// Print details about a given &DirEntry
 fn print_detailed(entry: &DirEntry) {
     if let Ok(metadata) = fs::metadata(entry.path()) {
-        println!("Name: {}", entry.file_name().to_str().unwrap());
+        println!("Name: {}", entry.file_name().to_str().expect("Failed to get file name"));
         println!("Size: {} bytes", metadata.len());
-        println!("Modified: {:?}", format_system_time(metadata.modified().unwrap()));
-        println!("Accessed: {:?}", format_system_time(metadata.accessed().unwrap()));
-        println!("Created: {:?}", format_system_time(metadata.created().unwrap()));
+        println!("Modified: {:?}", format_system_time(metadata.modified().expect("Failed to get modified time")));
+        println!("Accessed: {:?}", format_system_time(metadata.accessed().expect("Failed to get accessed time")));
+        println!("Created: {:?}", format_system_time(metadata.created().expect("Failed to get created time")));
     }
 }
 
+// Return System time as a RFC2822 string
 fn format_system_time(time: SystemTime) -> String {
     let datetime: DateTime<Utc> = time.into();
     datetime.to_rfc2822()
 }
 
-
 fn main() {
     let args = Args::parse();
 
-    // Parse the cli arguments
     let pattern = match args.pattern {
         Some(ref arg) => {
+            // Check if the provided pattern is valid RegEx
             match RegexBuilder::new(&arg).build() {
                 Ok(re) => re,
                 Err(_) => {
                     println!("Invalid RegEx!\n");
-                    use clap::CommandFactory;
                     let mut cmd = Args::command();
-                    match cmd.print_help() {
-                        Ok(help) => help,
-                        Err(e) => println!("Clap error: {}", e)
-                    };
+                    cmd.print_help().expect("Failed to print help");
                     return;
                 }
             }
@@ -85,14 +70,16 @@ fn main() {
         None => RegexBuilder::new(".*").build().unwrap(),
     };
 
+    // If path is not provided, set it to the working directory
     let path = match args.path {
         Some(arg) => arg,
-        // Gotta love safety
-        None => std::env::current_dir().expect("Failed to get working directory").to_string_lossy().into_owned(),
+        None =>
+            std::env::current_dir()
+                .expect("Failed to get working directory").to_string_lossy().to_string(),
     };
 
-    // Set "re" as the Regex pattern
     let mut json_paths = Vec::new();
+
     for entry_result in WalkDir::new(path)
         .into_iter()
         .filter_entry(|e| !is_hidden(e) || args.hidden) // Include hidden entries if args.hidden is true
@@ -102,33 +89,39 @@ fn main() {
             Err(e) => {
                 println!("{}", e);
                 continue;
-                // For each file in a directory/directories check whether an entry is valid and unwrap the result
             }
         };
 
         // Check if the pattern has been provided
         if args.pattern.is_some() {
-            if matches_requirements(&entry, &pattern) { // Check whether entry matches the Regex
-                println!("{}", entry.path().display()); // Show the entries full path
-                if !args.json { // Might hinder the performance a tiny bit, but I don't care
-                    if args.detail { // If we want to show details, then show the details ^^
+            // Check whether entry matches the Regex
+            if pattern.is_match(entry.file_name().to_str().unwrap()) {
+                println!("{}", entry.path().display()); // Print full path
+
+                if args.json {
+                    json_paths.push(entry.path().to_string_lossy().to_string());
+                } else {
+                    if args.detail {
                         print_detailed(&entry);
                     }
-                } else {
-                    json_paths.push(entry.path().to_string_lossy().to_string());
-                    // Push the path of every single result to the json thingy
                 }
             }
         } else {
-            if args.detail {
-                print_detailed(&entry);
+            if args.json {
+                json_paths.push(entry.path().to_string_lossy().to_string());
             } else {
-                println!("{}", entry.path().display());
+                if args.detail {
+                    print_detailed(&entry);
+                } else {
+                    // Print full path
+                    println!("{}", entry.path().display());
+                }
             }
         }
-    }
-    if args.json {
-        println!("{}", serde_json::to_string(&json_paths).unwrap())
-        // Output the json string
+
+        if args.json {
+            // Print results in JSON format
+            println!("{}", serde_json::to_string(&json_paths).expect("Failed to serialize to JSON"))
+        }
     }
 }
